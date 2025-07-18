0264ff77065157e22415c118394580800704445f
RATest35 <151275929+RATest35@users.noreply.github.com>
2025-07-17 19:40:59 -0500

    Fixed xml output, added comments to inventory_to_xml, init_database, and convert_to_binary functions.

--- app.py
+++ app.py
@@ -7,36 +7,52 @@ import io
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
 
 
+# Function to connect the database
 def init_database():
     with sqlite3.connect('inventory.db') as connection:
         with open('inventory_schema.sql') as f:
             connection.executescript(f.read())
 
 
+# Converts a file to binary
 def convert_to_binary(filename):
     with open(filename, 'rb') as f:
         blob_data = f.read()
     return blob_data
 
+
+# Takes the database table and downloads a xml file to the users computer
 @app.route('/xml-export')
-def inventory_to_xml(output_file='inventory.xml'):
+def inventory_to_xml():
     connnection = sqlite3.connect('inventory.db')
     cursor = connnection.cursor()
     cursor.execute('SELECT name, image, description, quantity, price FROM inventory')
+    # Stores all the rows of the database
     rows = cursor.fetchall()
     connnection.close()
+
+    '''
+    Creates a tree and creates a child node (item) for each item and then adds a child node (characteristics [name,
+    image, quantity, etc.]) for each column in a row. The value of the characteristic is stored in the
+    corresponding node
+    '''
     root = ET.Element('inventory')
     for row in rows:
         item_elmt = ET.SubElement(root, 'item')
         name_elmt = ET.SubElement(item_elmt, 'name')
         name_elmt.text = row[0]
         image_elmt = ET.SubElement(item_elmt, 'image')
+        '''
+        Have to check if an image is stored, for this item.
+        If an image is stored then have to convert from binary back into base64 
+        '''
         if row[1]:
             image_base64 = base64.b64encode(row[1]).decode('utf-8')
             image_elmt.text = image_base64
@@ -48,13 +64,14 @@ def inventory_to_xml(output_file='inventory.xml'):
         quantity_elmt.text = str(row[3])
         price_elmt = ET.SubElement(item_elmt, 'price')
         price_elmt.text = f'{row[4]:.2f}'
-    xml_bytes = io.BytesIO()
+
+    # Sets up output file
+    output_xml = io.BytesIO()
     tree = ET.ElementTree(root)
-    tree.write(output_file, encoding='utf-8', xml_declaration=True)
-    xml_bytes.seek(0)
+    tree.write(output_xml, encoding='utf-8', xml_declaration=True)
+    output_xml.seek(0)
 
-    return send_file(xml_bytes, mimetype='application/xml', as_attachment=True, download_name='inventory.xml')
-
+    return send_file(output_xml, mimetype='application/xml', as_attachment=True, download_name='inventory.xml')
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -77,7 +94,6 @@ def add():
             image_blob = convert_to_binary(temp_path)
             os.remove(temp_path)
 
-
         with sqlite3.connect('inventory.db') as items:
             cursor = items.cursor()
             cursor.execute('INSERT INTO INVENTORY (name, image, description, quantity, price) \
@@ -87,25 +103,27 @@ def add():
     else:
         return render_template('add.html')
 
+
 @app.route('/inventory')
 def inventory():
     conn = sqlite3.connect('inventory.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT name, description, image, quantity, price FROM inventory')
+    cursor.execute('SELECT name, image, description, quantity, price FROM inventory')
     rows = cursor.fetchall()
 
     data = []
     for row in rows:
-        name, description, image_blob, quantity, price = row
+        name, image_blob, description, quantity, price = row
         if image_blob:
             image_base64 = base64.b64encode(image_blob).decode('utf-8')
             image_uri = f"data:image/jpeg;base64,{image_base64}"
         else:
             image_uri = None  # or a default placeholder image path
 
-        data.append((name, description, image_uri, quantity, price))
+        data.append((name, image_uri, description, quantity, price))
 
     return render_template('inventory.html', data=data)
+
 
 if __name__ == '__main__':
     init_database()
